ffb6bf7e1557fd2c5d627cdecdda857338f64ade
hyperchi <hyperbolechi@gmail.com>
2017-01-22 14:47:45 -0500

    minor change

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,11 @@
-import Request from 'superagent'
+import Request from 'superagent';
 
-export const REQUEST_POSTS = 'REQUEST_POSTS'
-export const RECEIVE_POSTS = 'RECEIVE_POSTS'
-export const SELECT_REDDIT = 'SELECT_REDDIT'
-export const INVALIDATE_REDDIT = 'INVALIDATE_REDDIT'
+export const REQUEST_POSTS = 'REQUEST_POSTS';
+export const RECEIVE_POSTS = 'RECEIVE_POSTS';
+export const SELECT_REDDIT = 'SELECT_REDDIT';
+export const INVALIDATE_REDDIT = 'INVALIDATE_REDDIT';
 
-export const RECEIVE_SEARCH_RESULT = 'RECEIVE_SEARCH_RESULT'
+export const RECEIVE_SEARCH_RESULT = 'RECEIVE_SEARCH_RESULT';
 
 // request to call api for search
 export function getSearchResultRequest(key_words) {
